6a9c013f3b07a80c90b199aea93abea7fe23106e
中村明日香 <96176617+Appi66@users.noreply.github.com>
2022-05-15 10:35:37 +0900

    [Add]グローバルナビ固定

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
+// ページ内リンク
 $('#page-link a[href*="#"]').click(function () {
-  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-  var pos = $(elmHash).offset().top;  //idの上部の距離を取得
-  $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  var elmHash = $(this).attr('href'); 
+  var pos = $(elmHash).offset().top;  
+  $('body,html').animate({scrollTop: pos}, 500); 
   return false;
+});
+
+// グローバルナビ固定
+function FixedAnime() {
+  var headerH = $('#header').outerHeight(true);
+  var scroll = $(window).scrollTop();
+  if (scroll >= headerH){
+      $('#header').addClass('fixed');
+    }else{
+      $('#header').removeClass('fixed');
+    }
+}
+
+//ナビゲーションをクリックした際のスムーススクロール
+$('#g-navi a').click(function () {
+  var elmHash = $(this).attr('href'); 
+  var pos = Math.round($(elmHash).offset().top-120);  
+  $('body,html').animate({scrollTop: pos}, 500);
+  return false;
+});
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  FixedAnime();
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  FixedAnime();
 });
